4a1e9cb81c7d8cf376f84593573841027807ebf4
Adam Smith <anhsmith@gmail.com>
2022-12-07 15:37:51 +1300

    Creates package

---
+++ R/make_weightzero_contrasts.R
@@ -0,0 +1,21 @@
+#' Make weighted sum-to-zero contrasts for a factor
+#'
+#' @param x
+#'
+#' @return cont
+#' @export
+#'
+#' @examples
+#' x <- factor(rep(letters[1:3], times=4:6))
+#' make_weightzero_contrasts(x)
+make_weightzero_contrasts <- function(x) {
+  x = as.character(x)
+  n <- length(x)
+  levs <- unique(x)
+  nlevs <- length(levs)
+  cont <- array(0, c(n,nlevs-1), dimnames=list(NULL,levs[2:nlevs]))
+  cont[x == levs[1]] <- -1 / sum(x == levs[1])
+  for(i in levs[2:nlevs]) cont[x == i,i] <- 1 / sum(x == i)
+  cont <- data.frame(cont)
+  return(cont)
+}
